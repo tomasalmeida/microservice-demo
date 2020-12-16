f3aa6add36954fcee39f108ddcc1e6febb477b75
Tomás Dias Almeida <tomas.almeida@gmail.com>
2020-12-16 21:07:44 +0100

    Step 01 : initial commit from spring initializr

---
+++ src/test/java/com/demo/ms/search/DemoSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.ms.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
